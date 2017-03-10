23db88cf29fe0b982822af49a93a354c25b257ea
John Tucker <john@larkintuckerllc.com>
2017-03-09 19:45:55 -0500

    More array

---
+++ src/array-broken.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+const adder = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD':
+      state.push(action.value); // BREAKS PURE FUNCTION
+      return state; // BREAKS PURE FUNCTION
+    default:
+      return state;
+  }
+};
+const store = createStore(adder);
+let lastState = store.getState();
+store.subscribe(() => {
+  const newState = store.getState();
+  // eslint-disable-next-line
+  console.log(newState === lastState);
+  lastState = newState;
+});
+store.dispatch({ type: 'ADD', value: 'apple' });
+store.dispatch({ type: 'ADD', value: 'banana' });
+store.dispatch({ type: 'ADD', value: 'cranberry' });
---
+++ src/array-fixed.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const adder = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD':
+      return [...state, action.value];
+    default:
+      return state;
+  }
+};
+const store = createStore(adder);
+let lastState = store.getState();
+store.subscribe(() => {
+  const newState = store.getState();
+  // eslint-disable-next-line
+  console.log(newState === lastState);
+  lastState = newState;
+});
+store.dispatch({ type: 'ADD', value: 'apple' });
+store.dispatch({ type: 'ADD', value: 'banana' });
+store.dispatch({ type: 'ADD', value: 'cranberry' });
